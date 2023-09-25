20cff62e10af12540557d6c317423246c753f199
MihailFasteks <fastakovskiy.mihail04@gmail.com>
2023-09-25 08:16:47 +0300

    Initial Commit

---
+++ Car2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Car2
+//
+//  Created by Michalis on 25.09.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
